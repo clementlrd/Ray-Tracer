e08230139cdc7e8ab68af16722546947d5b872cd
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2022-11-22 11:47:16 +0100

    fix safe uint_8 add

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,12 +1,10 @@
 #include "utils.hpp"
 #include <cstdint>
 
-uint8_t safe_uint8_add(uint8_t a, uint8_t b)
-{
-  return (uint8_t)max(255, (int)a + (int)b);
+uint8_t safe_uint8_add(uint8_t a, uint8_t b) {
+  return (uint8_t)min(255, (int)a + (int)b);
 }
 
-uint8_t safe_uint8_sub(uint8_t a, uint8_t b)
-{
-  return (uint8_t)min(0, (int)a - (int)b);
+uint8_t safe_uint8_sub(uint8_t a, uint8_t b) {
+  return (uint8_t)max(0, (int)a - (int)b);
 }
