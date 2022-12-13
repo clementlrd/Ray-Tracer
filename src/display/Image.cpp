719d60d3bd15e4ddf82d6be921be221b5aa9a467
Antoine DOMINGUES <antoine.domingues@ensta-paris.fr>
2022-12-13 13:06:38 +0100

    add unique pointer format for pixels

--- src/display/Image.cpp
+++ src/display/Image.cpp
@@ -1,21 +1,17 @@
 #include "Image.hpp"
 
 Image::Image() : size{INIT_WIDTH, INIT_HEIGHT} {
-  Color baseColor = Color();
-  std::vector<Color> columns(size.height, baseColor);
-  image.resize(size.width, columns);
+  image = std::make_unique<Color[]>(size.height * size.width);
 }
 
 Image::Image(size_t width, size_t height) : size{width, height} {
-  Color baseColor = Color();
-  std::vector<Color> columns(height, baseColor);
-  image.resize(width, columns);
+  image = std::make_unique<Color[]>(size.height * size.width);
 }
 
 Image::Image(size_t width, size_t height, Color baseColor)
     : size{width, height} {
-  std::vector<Color> columns(height, baseColor);
-  image.resize(width, columns);
+  image = std::make_unique<Color[]>(size.height * size.width);
+  // To do : set base color everywhere
 }
 
 screenSize const &Image::getSize() const { return size; }
@@ -28,31 +24,5 @@ void Image::getSize(size_t &w, size_t &h) const {
 size_t Image::getWidth() const { return size.width; }
 
 size_t Image::getHeight() const { return size.height; }
-
-Color &Image::getColor(size_t posW, size_t posH) { return image[posW][posH]; }
-
-screenSize const &Image::changeSize(screenSize const &final_size) {
-  for (size_t i = 0; i < final_size.width; i++) {
-    image[i].resize(final_size.height, Color());
-  };
-  Color baseColor = Color();
-  std::vector<Color> columns(final_size.height, baseColor);
-  image.resize(final_size.width, columns);
-  size.width = final_size.width;
-  size.height = final_size.height;
-  return size;
-}
-
-screenSize const &Image::changeSize(size_t width, size_t height) {
-  for (size_t i = 0; i < width; i++) {
-    image[i].resize(height, Color());
-  };
-  Color baseColor = Color();
-  std::vector<Color> columns(height, baseColor);
-  image.resize(width, columns);
-  size.width = width;
-  size.height = height;
-  return size;
-}
 
 size_t Image::getResolution() const { return size.width * size.height; }
--- src/display/Image.hpp
+++ src/display/Image.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "../materials/color.hpp"
-#include <vector>
+#include <algorithm>
+#include <memory>
 
 #define INIT_WIDTH 600
 #define INIT_HEIGHT 400
@@ -20,21 +21,39 @@ typedef struct screenSize screenSize;
 class Image {
 protected:
   screenSize size;
-  std::vector<std::vector<Color>> image;
+  std::unique_ptr<Color[]> image;
 
 public:
   Image();
   Image(size_t width, size_t height);
   Image(size_t width, size_t height, Color baseColor);
+  Image(const Image &source)
+      : size(source.size),
+        image(std::make_unique<Color[]>(size.width * size.height)) {
+    std::copy_n(source.image.get(), size.width * size.height, image.get());
+  }
+  Image(Image &&source) : size(source.size), image(source.image.release()) {}
+  Image &operator=(const Image &source) {
+    size = source.size;
+    image = std::make_unique<Color[]>(size.width * size.height);
+    std::copy_n(source.image.get(), size.width * size.height, image.get());
+    return *this;
+  }
+  Image &operator=(Image &&source) {
+    size = source.size;
+    image.swap(source.image);
+    source.image.release();
+    return *this;
+  }
 
   screenSize const &getSize() const;
   void getSize(size_t &w, size_t &h) const;
   size_t getWidth() const;
   size_t getHeight() const;
-  Color &getColor(size_t posW, size_t posH);
+  Color &operator()(size_t x, size_t y) const {
+    return *(image.get() + x * size.height + y);
+  };
 
-  screenSize const &changeSize(screenSize const &final_size);
-  screenSize const &changeSize(size_t width, size_t height);
   size_t getResolution() const;
   //~Image();
 };
