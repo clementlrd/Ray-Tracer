822c26265c79e199620f3f9745b65eb6d4414732
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2022-12-13 14:28:25 +0100

    add vector's utility function

--- src/maths/Vector3D.cpp
+++ src/maths/Vector3D.cpp
@@ -5,6 +5,7 @@
 Vect3D::Vect3D() : vect{0, 0, 0} {}
 Vect3D::Vect3D(double p) : vect{p, p, p} {}
 Vect3D::Vect3D(double x, double y, double z) : vect{x, y, z} {}
+Vect3D::Vect3D(const Vect3D &v) : Vect3D(v.x(), v.y(), v.z()) {}
 
 double Vect3D::x() const { return vect[0]; }
 double Vect3D::y() const { return vect[1]; }
@@ -12,15 +13,15 @@ double Vect3D::z() const { return vect[2]; }
 double Vect3D::operator[](int i) const { return vect[i]; }
 
 Vect3D &Vect3D::operator+=(const Vect3D &vect2) {
-  vect[0] += vect2[0];
-  vect[1] += vect2[1];
-  vect[2] += vect2[2];
+  vect[0] += vect2.x();
+  vect[1] += vect2.y();
+  vect[2] += vect2.z();
   return *this;
 }
 Vect3D &Vect3D::operator-=(const Vect3D &vect2) {
-  vect[0] -= vect2[0];
-  vect[1] -= vect2[1];
-  vect[2] -= vect2[2];
+  vect[0] -= vect2.x();
+  vect[1] -= vect2.y();
+  vect[2] -= vect2.z();
   return *this;
 }
 Vect3D &Vect3D::operator*=(double t) {
@@ -36,5 +37,5 @@ Vect3D &Vect3D::operator/=(double t) {
 
 double Vect3D::norm() const { return std::sqrt(norm_squared()); }
 double Vect3D::norm_squared() const {
-  return vect[0] * vect[0] + vect[1] * vect[1] + vect[2] * vect[2];
+  return x() * x() + y() * y() + z() * z();
 }
--- src/maths/Vector3D.hpp
+++ src/maths/Vector3D.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <cmath>
+#include <ostream>
+
+/* Vector 3D class */
 
 class Vect3D {
 protected:
@@ -10,6 +13,7 @@ public:
   Vect3D();
   Vect3D(double);
   Vect3D(double x, double y, double z);
+  Vect3D(const Vect3D &);
 
   double x() const;
   double y() const;
@@ -24,3 +28,34 @@ public:
   double norm() const;
   double norm_squared() const;
 };
+
+/* Utility functions */
+
+inline std::ostream &operator<<(std::ostream &out, const Vect3D &u) {
+  return out << "[" << u.x() << ' ' << u.y() << ' ' << u.z() << "]";
+}
+
+inline Vect3D operator+(const Vect3D &u, const Vect3D &v) {
+  return Vect3D(u.x() + v.x(), u.y() + v.y(), u.z() + v.z());
+}
+
+inline Vect3D operator-(const Vect3D &u, const Vect3D &v) {
+  return Vect3D(u.x() - v.x(), u.y() - v.y(), u.z() - v.z());
+}
+
+inline Vect3D operator*(const Vect3D &u, double t) {
+  return Vect3D(u.x() * t, u.y() * t, u.z() * t);
+}
+
+inline Vect3D operator/(const Vect3D &u, double t) { return u * (1 / t); }
+
+inline double dot(const Vect3D &u, const Vect3D &v) {
+  return u.x() * v.x() + u.y() * v.y() + u.z() * v.z();
+}
+
+inline Vect3D cross(const Vect3D &u, const Vect3D &v) {
+  return Vect3D(u.y() * v.z() - u.z() * v.y(), u.z() * v.x() - u.x() * v.z(),
+                u.x() * v.y() - u.y() * v.x());
+}
+
+inline Vect3D unit(const Vect3D &u) { return u / u.norm(); }
