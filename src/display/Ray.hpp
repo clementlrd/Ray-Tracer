8a45875c2ffe70c91527a63ea1635b48b3f21529
Antoine DOMINGUES <antoine.domingues@ensta-paris.fr>
2022-12-23 14:25:37 +0100

    compute a basic header of ray

--- src/display/Ray.hpp
+++ src/display/Ray.hpp
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "../maths/Vector3D.hpp"
+
+class Ray {
+private:
+  Point3D origin;
+  Vect3D direction;
+
+public:
+  Ray();
+  Ray(const Point3D &origin, const Vect3D &direction);
+  Ray(const Ray &);
+
+  Point3D get_origin();
+  Vect3D get_direction();
+
+  Point3D current_pos(double time);
+};
