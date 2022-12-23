e7eb92932f268b302be141ad86087117bdb6abd5
Antoine DOMINGUES <antoine.domingues@ensta-paris.fr>
2022-12-23 13:14:56 +0100

    create headers of new objects to set up the ray tracer

---
+++ src/Scene/Scene.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+//Scene is a collection of objects
---
+++ src/Scene/objects.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+/* TO DO :
+Create an abstract base class and then create objects that can be added to the scene (sphere, plan, cube...)
+
+Each object can have the possibility to emit light.
+
+Add texture to each object
+*/
