278a78441274d07a5257873acafdb22d47e20320
Antoine DOMINGUES <antoine.domingues@ensta-paris.fr>
2022-12-23 19:30:34 +0100

    add normal and tangent on objects and naming convention

--- src/Scene/objects.hpp
+++ src/Scene/objects.hpp
@@ -13,11 +13,13 @@ protected:
 public:
   virtual double area() const = 0;
   virtual double volume() const = 0;
+  virtual Vect3D normal(Point3D surface_pt) const = 0;
+  virtual Vect3D tangent(Point3D surface_pt) const = 0;
 };
 
 class Cube : public Object {
 private:
-  // The three vectors that have length = Object.length and define a cube
+  // The three length of vectors are Object.length
   Vect3D vectx;
   Vect3D vecty;
   Vect3D vectz;
@@ -30,11 +32,13 @@ public:
 
   double area(void) const { return 6 * length * length; }
   double volume(void) const { return length * length * length; }
-  double get_side() const { return length; }
+  double getSide() const { return length; }
+  Vect3D normal(Point3D surface_pt) const;
+  Vect3D tangent(Point3D surface_pt) const;
 };
 
 class Sphere : public Object {
-  // The length corresponds to the radius
+  // The length here corresponds to the radius
 public:
   Sphere();
   Sphere(Point3D origin, double radius);
@@ -42,17 +46,17 @@ public:
 
   double area(void) const { return 4 * pi * length * length * length / 3; }
   double volume(void) const { return 4 * pi * length * length; }
-  double get_radius() const { return length; }
+  double getRadius() const { return length; }
+  Vect3D normal(Point3D surface_pt) const;
+  Vect3D tangent(Point3D surface_pt) const;
 };
 
 class Plane : public Object {
   // The length corresponds to the radius
 private:
-  // With length = length
-  Vect3D vectx;
+  Vect3D vectx; // Which length = length
   double width;
-  // With length = width
-  Vect3D vecty;
+  Vect3D vecty; // Which length = width
 
 public:
   Plane();
@@ -62,8 +66,10 @@ public:
 
   double area(void) const { return length * width; }
   double volume(void) const { return 0; }
-  double get_length() const { return length; }
-  double get_width() const { return width; }
+  double getLength() const { return length; }
+  double getWidth() const { return width; }
+  Vect3D normal(Point3D surface_pt) const;
+  Vect3D tangent(Point3D surface_pt) const;
 };
 /* TO DO :
 [DONE] Create an abstract base class and then create objects that can be added
