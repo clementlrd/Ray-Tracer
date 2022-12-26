b605bf588ffc55386bc2c038870448e8e6d5ae0d
Antoine DOMINGUES <antoine.domingues@ensta-paris.fr>
2022-12-26 19:19:13 +0100

    first test of ray tracer

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,16 +1,37 @@
+#include "color.hpp"
 #include "image.hpp"
+#include "maths.hpp"
+#include "object.hpp"
+#include "ray.hpp"
 
 #include <iostream>
 
+using maths::Vect3D;
+using obj::Camera;
+
 void print_image(Image const& image);
+Color ray_color(const Ray& r);
 
 int main() {
-  Image img1(2, 2);
-  Color c2(ColorRGB(255, 128, 64));
-  img1(0, 0) = c2;
-  img1(1, 0) = c2;
+  // Init image and camera
+  Image img1  = Image();
+  Camera cam1 = Camera();
+  int height  = (int)img1.getHeight();
+  int width   = (int)img1.getWidth();
+  for (int j = height - 1; j >= 0; --j) {
+    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+    for (int i = 0; i < width; ++i) {
+      double u = double(i) / (width - 1);
+      double v = double(j) / (height - 1);
+      // Create a ray according to the position of the camera
+      Ray r(cam1.get_origin(),
+            cam1.get_llc() + u * cam1.get_horizontal() + v * cam1.get_vertical()
+                - cam1.get_origin());
+      Color color_pixel = ray_color(r);
+      img1(i, j)        = color_pixel;
+    };
+  };
   printer::ppm(img1);
-
   return 0;
 }
 
@@ -20,4 +41,11 @@ void print_image(Image const& image) {
       std::cout << image(i, j).repr() << std::endl;
     }
   }
+}
+
+Color ray_color(const Ray& r) {
+  Vect3D unit_direction = unit(r.get_direction());
+  double t              = 0.5 * (unit_direction.y() + 1.0);
+  return Color({ 255, 255, 255 }) * (1 - t)
+         + Color({ 128, 179, 255 }) * t; // from white to cyan
 }
