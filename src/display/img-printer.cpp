59e7d4d6543e5913c19b64b25eb508a66afbacce
Antoine DOMINGUES <antoine.domingues@ensta-paris.fr>
2022-12-26 20:07:06 +0100

    correct printer (orientation)

--- src/display/img-printer.cpp
+++ src/display/img-printer.cpp
@@ -7,7 +7,7 @@ void printer::ppm(Image image) {
   std::cout << "P3\n"
             << currentSize.width << ' ' << currentSize.height << "\n255\n";
 
-  for (size_t j = 0; j < currentSize.height; j++) {
+  for (size_t j = currentSize.height; j > 0; j--) {
     for (size_t i = 0; i < currentSize.width; i++) {
       Color current_color = image(i, j);
       const RGB_color rgb = current_color.getRGBColor();
