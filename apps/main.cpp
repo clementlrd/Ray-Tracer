5a4ab110c8dc16d0c1615f63815eb0ff9b1f9ec5
Antoine DOMINGUES <antoine.domingues@ensta-paris.fr>
2022-12-27 11:57:24 +0100

    avoid using

--- apps/main.cpp
+++ apps/main.cpp
@@ -6,18 +6,15 @@
 
 #include <iostream>
 
-using maths::Vect3D;
-using obj::Camera;
-
 void print_image(Image const& image);
 Color ray_color(const Ray& r);
 
 int main() {
   // Init image and camera
-  Image img1  = Image();
-  Camera cam1 = Camera();
-  int height  = (int)img1.getHeight();
-  int width   = (int)img1.getWidth();
+  Image img1       = Image();
+  obj::Camera cam1 = obj::Camera();
+  int height       = (int)img1.getHeight();
+  int width        = (int)img1.getWidth();
   for (int j = height - 1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < width; ++i) {
